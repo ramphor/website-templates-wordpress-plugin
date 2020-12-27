cb73056fc9f0030693416c2ace28358dd87e73ca
Puleeno Nguyen <puleeno@gmail.com>
2020-12-28 01:37:14 +0700

    Create template loader

---
+++ includes/WebsiteTemplate/TemplateLoader.php
@@ -0,0 +1,40 @@
+<?php
+namespace Ramphor\WebsiteTemplate;
+
+use Jankx\Template\Template;
+
+class TemplateLoader
+{
+    protected static $loader;
+
+    public static function getLoader()
+    {
+        if (is_null(static::$loader)) {
+            $templateDirectory = sprintf('%s/templates', dirname(WP_WEBSITE_TEMPLATES_PLUGIN_FILE));
+            static::$loader = Template::getLoader(
+                $templateDirectory,
+                apply_filters('wp_website_template_override_directory', 'templates/website'),
+                'wordpress'
+            );
+        }
+        return static::$loader;
+    }
+
+    public static function search()
+    {
+        $args = func_get_args();
+        return call_user_func_array(array(
+            static::getLoader(),
+            'searchTemplate'
+        ), $args);
+    }
+
+    public static function render()
+    {
+        $args = func_get_args();
+        return call_user_func_array(array(
+            static::getLoader(),
+            'render'
+        ), $args);
+    }
+}
