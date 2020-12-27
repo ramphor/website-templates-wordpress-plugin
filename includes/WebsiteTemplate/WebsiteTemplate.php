2488be87b4f457965fe9baadfffbe4900ee158e0
Puleeno Nguyen <puleeno@gmail.com>
2020-12-28 01:13:48 +0700

    Init project structure

---
+++ includes/WebsiteTemplate/WebsiteTemplate.php
@@ -0,0 +1,5 @@
+<?php
+namespace Ramphor\WebsiteTemplate;
+
+class WebsiteTemplate {
+}
---
+++ wp-website-templates.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WP Website Templates
+ * Plugin URI: https://github.com/ramphor/website-templates-wordpress-plugin
+ * Description: Create website templates for WordPress website
+ * Version: 0.0.1
+ * Author: Ramphor Premium
+ * Author URI: https://puleeno.com
+ */
+
+if (!defined('WP_WEBSITE_TEMPLATES_PLUGIN_FILE')) {
+    define('WP_WEBSITE_TEMPLATES_PLUGIN_FILE', __FILE__);
+}
+
