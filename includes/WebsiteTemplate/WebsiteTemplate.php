200d7c61f13c1eb37089b11e6c1deed16266dc45
Puleeno Nguyen <puleeno@gmail.com>
2020-12-28 01:19:42 +0700

    Load the plugin

--- includes/WebsiteTemplate/WebsiteTemplate.php
+++ includes/WebsiteTemplate/WebsiteTemplate.php
@@ -1,5 +1,19 @@
 <?php
 namespace Ramphor\WebsiteTemplate;
 
-class WebsiteTemplate {
+class WebsiteTemplate
+{
+    protected static $instance;
+
+    public static function get_instance()
+    {
+        if (is_null(static::$instance)) {
+            static::$instance = new static();
+        }
+        return static::$instance;
+    }
+
+    protected function __construct()
+    {
+    }
 }
--- wp-website-templates.php
+++ wp-website-templates.php
@@ -8,7 +8,26 @@
  * Author URI: https://puleeno.com
  */
 
-if (!defined('WP_WEBSITE_TEMPLATES_PLUGIN_FILE')) {
-    define('WP_WEBSITE_TEMPLATES_PLUGIN_FILE', __FILE__);
+use Ramphor\WebsiteTemplate\WebsiteTemplate;
+
+if ( ! defined( 'WP_WEBSITE_TEMPLATES_PLUGIN_FILE' ) ) {
+	define( 'WP_WEBSITE_TEMPLATES_PLUGIN_FILE', __FILE__ );
 }
 
+$composer_autload = sprintf( '%s/vendor/autoload.php', dirname( __FILE__ ) );
+if ( file_exists( $composer_autload ) ) {
+	require_once $composer_autload;
+}
+
+// If class WebsiteTemplate is not found this plugin is incorrect. So the process load the plugin will be stopped
+if ( ! class_exists( WebsiteTemplate::class ) ) {
+	return;
+}
+
+if ( ! function_exists( 'wp_website_templates' ) ) {
+	function wp_website_templates() {
+		return WebsiteTemplate::get_instance();
+	}
+}
+
+$GLOBALS['wpwt'] = wp_website_templates();
